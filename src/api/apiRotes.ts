83b1686714ed92fff65784976b2cf64642c4d313
GuilhermeOlavoLaux <guilherme.laux@compliancetotal.com.br>
2022-04-17 23:00:12 -0300

    att

--- src/api/apiRotes.ts
+++ src/api/apiRotes.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3030/'
+  baseURL: 'https://trello-back-deploy-jmfiuj93c-guilhermeolavolaux.vercel.app/'
 })
 
 export { api }
